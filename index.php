cc1ab021e32e6b5971407c8b92a050fb604f4d29
loganDag <logandag11@gmail.com>
2025-12-29 05:03:49 -0600

    Started to add content

--- index.php
+++ index.php
@@ -5,8 +5,15 @@
 <?php
 require "includes/bootstrap.html";
 ?>
+<title>Christian Social Site | Login</title>
     </head>
     <body>
-        
+        <div class="hero-greeting">
+        <h1 class="hero-greeting-header">Christian Social Site</h1>
+        <p class="fs-5 text-muted">A social site for Christians and all!</p>    
+    </div>
+        <main>
+            <p class="fs-4 text-muted"></p>
+        </main>
     </body>
 </html>
