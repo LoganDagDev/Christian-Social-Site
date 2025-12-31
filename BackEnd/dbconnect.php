5e4bebad07bb910c25ab5a20886cdb2833ac43b9
loganDag <logandag11@gmail.com>
2025-12-30 23:53:45 -0600

    Added the connection variable

--- BackEnd/dbconnect.php
+++ BackEnd/dbconnect.php
@@ -3,10 +3,13 @@
 Will have info and work on the "live" production version of the website.
 Variables and code here will be blank for security reasons.
 */
-$username = '';
-$password = '';
+$db_username = '';
+$db_password = '';
 $db_name = '';
 $db_host = '';
 
-$SQL = '';
+$conn = new mysqli("$db_host", "$db_username", "$db_password", "$db_name");
+if ($conn->connect_error)
+{
+ }
 ?>
